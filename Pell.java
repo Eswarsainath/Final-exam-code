6d728b35f21df39e8c3563aae766751c77fedbef
Eswarsainath <65670272+Eswarsainath@users.noreply.github.com>
2021-12-15 16:06:33 -0500

    Update Pell.java

--- Pell.java
+++ Pell.java
@@ -1,3 +1,5 @@
+package pell;
+
 public class Pell {
     public Pell() {
     }
@@ -5,18 +7,23 @@ public class Pell {
     public long get(int n) {
         if (n < 0) throw new UnsupportedOperationException("Pell.get is not supported for negative n");
 
-        if (n<=2)
-        {
+        if(n<=2)
             return n;
+        long[] dp = new long[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i =3;i<=n;i++)
+        {
+            dp[i] = 2*dp[i-1] + dp[i-2];
         }
-        return 2*get(n-1)+get(n-2);
+        return dp[n];
     }
 
-    public static void main(String args[])
-    {
-        int n = 50;
+    public static void main(String args[]) {
+        int n = 5;
         Pell pell = new Pell();
         System.out.println(pell.get(n));
     }
 }
-
